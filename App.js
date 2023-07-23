9ac205fe3f6c34f574b0b2303e1bec88419c9e18
Vaibhav Agrawal <vaibhav.ag464@gmail.com>
2023-07-23 23:08:57 +0530

    Added ScrollBar Animation & Responsive

---
+++ App.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        entry.target.classList.toggle('show', entry.isIntersecting);
+        // if (entry.isIntersecting) {
+        //     entry.target.classList.add('show');
+        // }
+        // else {
+        //     entry.target.classList.remove('show');
+        // }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.vertical');
+hiddenElements.forEach((el) => observer.observe(el));
