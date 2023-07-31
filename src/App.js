0029246f020b5abf5b6f55d9823a5114d878fd25
Vaibhav Agrawal <vaibhav.ag464@gmail.com>
2023-07-31 22:46:04 +0530

    Back to top btn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Scroll Animation
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -13,3 +14,20 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.app-info, .vertical');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Back to top animation
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
